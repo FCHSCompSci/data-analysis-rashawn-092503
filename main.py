2293228f43b498ceb751d0101e8dc678ce8bdb02
Rashawn butler <rashawn.butler@stu.jefferson.kyschools.us>
2018-12-19 08:06:41 -0500

    Finally done

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ chess_wins = '2016_CvH.csv'
 white_winner = 0
 black_winner = 0
 draws = 0
-white_clock = []
-black_clock = []
 with open(chess_wins) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -22,11 +20,9 @@ with open(chess_wins) as f:
 x = []
 lsts = ["white wins", "black wins", "draw"]
 performance = [white_winner,black_winner,draws]
-
 plt.bar(lsts, performance, align='center', alpha=0.5, color="red")
 plt.xticks(lsts, rotation=20)
 plt.ylabel('Games', fontsize=16)
 plt.xlabel("Winners", fontsize=16)
 plt.title('Wins and draws of all 2016 chess official competitions')
-
 plt.show()
