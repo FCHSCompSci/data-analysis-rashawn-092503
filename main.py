ba6ebe8f71eb2178a0b731259dca4d59bfa2a22e
Rashawn butler <rashawn.butler@stu.jefferson.kyschools.us>
2018-12-19 08:05:15 -0500

    Finally done

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
 import matplotlib.pyplot as plt
-print("Hello world")
+import csv
+
+chess_wins = '2016_CvH.csv'
+white_winner = 0
+black_winner = 0
+draws = 0
+white_clock = []
+black_clock = []
+with open(chess_wins) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    for row in reader:
+        if row[17] == "White":
+            white_winner += 1
+        elif row[17] == "Black":
+            black_winner += 1
+        elif row[17] == "Draw":
+            draws += 1
+    print(white_winner)
+    print(black_winner)
+x = []
+lsts = ["white wins", "black wins", "draw"]
+performance = [white_winner,black_winner,draws]
+
+plt.bar(lsts, performance, align='center', alpha=0.5, color="red")
+plt.xticks(lsts, rotation=20)
+plt.ylabel('Games', fontsize=16)
+plt.xlabel("Winners", fontsize=16)
+plt.title('Wins and draws of all 2016 chess official competitions')
+
+plt.show()
